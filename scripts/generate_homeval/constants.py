e322689200ec6c0a93e6bc93550c8b37177027f7
Jean Cochrane <jean@jeancochrane.com>
2026-02-09 14:16:05 -0600

    Switch to draft versions of 2026 core tables for test deployment

--- scripts/generate_homeval/constants.py
+++ scripts/generate_homeval/constants.py
@@ -2,6 +2,8 @@
 
 # It's helpful to factor these tables out into shared constants because we often
 # need to switch to dev tables for testing
-HOMEVAL_ASSESSMENT_CARD_TABLE = "pinval.assessment_card"
-HOMEVAL_COMP_TABLE = "pinval.comp"
+HOMEVAL_ASSESSMENT_CARD_TABLE = "z_ci_jeancochrane_update_pinval_model_run_with_2026_wip_runs_pinval.assessment_card"
+HOMEVAL_COMP_TABLE = (
+    "z_ci_jeancochrane_update_pinval_model_run_with_2026_wip_runs_pinval.comp"
+)
 HOMEVAL_DATA_DICT_TABLE = "pinval.vars_dict"
