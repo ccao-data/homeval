66e21baa1499dd36dda2c594be9c9485261df941
Michael Wagner <wagnerleemichael@gmail.com>
2026-08-03 13:30:14 -0500

    Update dev tables

--- scripts/generate_homeval/constants.py
+++ scripts/generate_homeval/constants.py
@@ -2,6 +2,8 @@
 
 # It's helpful to factor these tables out into shared constants because we often
 # need to switch to dev tables for testing
-HOMEVAL_ASSESSMENT_CARD_TABLE = "pinval.assessment_card"
-HOMEVAL_COMP_TABLE = "pinval.comp"
+HOMEVAL_ASSESSMENT_CARD_TABLE = (
+    "z_ci_edit_assets_for_2026_comps_refresh_pinval.assessment_card"
+)
+HOMEVAL_COMP_TABLE = "z_ci_edit_assets_for_2026_comps_refresh_pinval.comp"
 HOMEVAL_DATA_DICT_TABLE = "pinval.vars_dict"
